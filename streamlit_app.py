f7fd8cdf2ff0370d998a0d6f496d13d0a4b8b681
zaedaneaflah <168023153+zaedaneaflah@users.noreply.github.com>
2024-04-25 03:12:57 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,8 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib as mpl
 import seaborn as sns
 import streamlit as st
-import os
 
 # sidebar pada streamlit
 st.sidebar.subheader("Data Pribadi")
